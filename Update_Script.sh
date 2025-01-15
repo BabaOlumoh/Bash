347daba7c748303745ef48ae470bbf3829daf1be
BabaOlumoh <olumohbabatunde@gmail.com>
2025-01-15 23:25:16 +0000

    15/01/2025

--- Update_Script.sh
+++ Update_Script.sh
@@ -4,15 +4,28 @@ release_file=/etc/os-release
 logfile=~/Boring_Stuff/Bash/logFiles/updater.log
 errorlog=~/Boring_Stuff/Bash/logFiles/updater_errors.log
 
+exit_status(){
+    if [ $? -ne 0 ]
+    then
+        echo "An error occurred, please check $updater_errors.log."
+    fi 
+}
+
 if grep -q "arch" $release_file
 then
     # For host on Arch 
-    sudo pacman -Syu
+    sudo pacman -Syu >$logfile 2>$errorlog
+    exit_status
 elif grep -q "debian" $release_file || grep -q "ubuntu" $release_file
 then
+    echo "This script ran on the $(date)" >>$logfile 
+
     # For Debian or Ubuntu host
-    sudo apt update
-    sudo apt dist-upgrade
+    sudo apt update >>$logfile 2>>$errorlog
+    exit_status
+
+    sudo apt dist-upgrade -y >>$logfile 2>>$errorlog
+    exit_status
 else
     echo "No host found"
 fi
